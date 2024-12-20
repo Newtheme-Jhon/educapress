f7c477381547569b8e374aa7de7cec9c650c700c
Newtheme-Jhon <124293665+Newtheme-Jhon@users.noreply.github.com>
2024-12-20 13:45:04 +0100

    Update app.php
    deedede

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\Cors;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -24,7 +25,10 @@ return Application::configure(basePath: dirname(__DIR__))
     }
 )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except: [
+            env('APP_URL') . '/researchjspost'
+        ]);
+        $middleware->append(Cors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
