5b85a89ac400beb3fadd6b12abb8d20ed9d5dd11
Newtheme-Jhon <124293665+Newtheme-Jhon@users.noreply.github.com>
2024-12-22 20:40:17 +0100

    Update banner-bulletpoints-back.blade.php

--- resources/views/includes/banner-bulletpoints-back.blade.php
+++ resources/views/includes/banner-bulletpoints-back.blade.php
@@ -4,7 +4,7 @@
             <img src="{{ asset('img/welcome/ia-foto-home.jpg') }}" alt="cursos ia">
         </div>
         <div>
-            <h3 class="text-3xl mb-2 font-kumb">¿Te Vas A Quedar Atrás?</h3>
+            <h3 class="text-2xl mb-2 font-kumb">¿Te Vas A Quedar Atrás?</h3>
             <p class="mb-3 text-lg">No lo pienses más, empieza uno de nuestros cursos gratuitos y mejora tus skills que te ayudaran a ser más productivo en un entorno de trabajo desde casa.
             <p class="mb-1"><img class="inline" src="{{asset('/img/welcome/check.png')}}" alt="check icon"> Clases en vídeo</p>
             <p class="mb-1"><img class="inline" src="{{asset('/img/welcome/check.png')}}" alt="check icon"> Cursos gratis</p>
